3edd599c7a905e0fad1d489f2aa6a9cb3c693101
Bochkarev90 <bochkarev.rabota@gmail.com>
2019-01-26 17:59:27 +0300

    Написал кривую Коха

---
+++ kokh.py
@@ -0,0 +1,30 @@
+import turtle
+
+turtle.speed('fastest')
+turtle.left(180)
+turtle.penup()
+turtle.forward(1000)
+turtle.left(180)
+turtle.pendown()
+
+
+def kokh(length:int, depth:int):
+    if depth == 1:
+        turtle.forward(length)
+        turtle.left(45)
+        turtle.forward(length)
+        turtle.right(90)
+        turtle.forward(length)
+        turtle.left(45)
+        turtle.forward(length)
+        return
+    kokh(length, depth - 1)
+    turtle.left(45)
+    kokh(length, depth - 1)
+    turtle.right(90)
+    kokh(length, depth - 1)
+    turtle.left(45)
+    kokh(length, depth - 1)
+
+
+kokh(2, 7)
