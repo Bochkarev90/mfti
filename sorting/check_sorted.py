c4024714a3658a4884042079f99f622d8274e02c
Bochkarev90 <bochkarev.rabota@gmail.com>
2019-02-05 15:30:08 +0300

    Написал функцию проверки отсортированности массива

---
+++ sorting/check_sorted.py
@@ -0,0 +1,27 @@
+def check_sorted(a: list, ascending=True):
+    if ascending is True:
+        for i in range(0, len(a)-1):
+            if a[i] > a[i+1]:
+                return False
+        return True
+    for i in range(0, len(a)-1):
+        if a[i] < a[i+1]:
+            return False
+        return True
+
+
+def reverse_list(a: list):
+    k = len(a)
+    for i in range(k // 2):
+        a[k-1], a[i] = a[i], a[k-1]
+        k -= 1
+    return a
+
+
+new_list = [1, 3, 4, 7, 19, 32, 43, 50, 59, 61, 78, 79, 80, 85, 92, 99]
+print(new_list)
+print(check_sorted(new_list))
+reversed_list = reverse_list(new_list)
+print(reversed_list)
+print(check_sorted(reversed_list))
+print(check_sorted(reversed_list, False))
